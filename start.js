5c7c28f9226e38398ee191c82ca20de45aa93560
6Morpheus6 <wanton-decay@gmx.net>
2025-10-23 06:23:24 +0200

    update

--- start.js
+++ start.js
@@ -1,20 +1,23 @@
 module.exports = {
   daemon: true,
   run: [{
-    "method": "shell.run",
-    "params": {
-      "path": "app",
-      "env": {
+    method: "shell.run",
+    params: {
+      path: "app",
+      env: {
         "PYTORCH_ENABLE_MPS_FALLBACK": "1"
       },
-      "venv": "env",
-      "message": "python app.py",
-      "on": [{ "event": "/http:\/\/[0-9.:]+/", "done": true }]
+      venv: "env",
+      message: "python app.py",
+      on: [{
+        event: "/http:\/\/[0-9.:]+/",
+        done: true
+      }]
     }
   }, {
-    "method": "local.set",
-    "params": {
-      "url": "{{input.event[0]}}"
+    method: "local.set",
+    params: {
+      url: "{{input.event[0]}}"
     }
   }]
 }
